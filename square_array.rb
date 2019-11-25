4e7f65c6ba4b44aa3461eea7b24508904a1d8ecb
kenneth gil <astronori@gmail.com>
2019-11-25 15:19:54 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
-  # your code here
+def squared_array
+  enum = arr.each
+  a = []
+  loop do
+    n = enum.next
+    a << n*n
+  end
+  a
 end
+square [1, 2, 3]
+  #=> [1, 4, 9]
